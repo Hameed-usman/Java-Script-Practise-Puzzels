623f53fe94d463a1896a84b060f88dfc0ae5d4ad
Hameed-usman <usman.hameed1145@gmail.com>
2024-07-04 20:00:02 +0500

    correct

---
+++ script.js
@@ -0,0 +1,61 @@
+                            // REMOVE EXTRA SPACES
+
+// let extract = "     Hello World     ";
+// let trimExtract = extract.trim();
+// console.log(trimExtract);
+
+
+                            // SUB STRING IN JAVA SCRIPT
+
+
+// const alpha = "Hello World";
+// const beta = alpha.substring(5,10);
+// console.log(beta);
+
+
+                       //  EXTRACTION OF CHARACTER USING SUB STRING
+
+
+// let delta = "Hello World";
+// let gama = delta.substring(0, 5);
+// console.log(gama); 
+
+
+                            // SPLITING METHOD
+
+// let spl = "hello-world";
+// let arr = spl.split("-");
+// console.log(arr); 
+
+
+                            // JOIN METHOD
+
+// let jn = ["hello", "world"];
+// let alpha = jn.join(" ");
+// console.log(alpha); 
+
+
+                         // INDEX FINDING 
+
+// let beta = "Hello World";
+// let index = beta.indexOf("o");
+// console.log(index); 
+
+
+                                  // CHECK STATEMENT
+
+// let alpha = "World";
+// let startsWith = alpha.startsWith("Wor");
+// console.log(startsWith);
+
+// let beta = "Hello World";
+// let endsWith = beta.endsWith("World");
+// console.log(endsWith);
+
+// let delta = "Hello";
+// let includesChar = delta.includes("o");
+// console.log(includesChar);
+
+
+
+                    
